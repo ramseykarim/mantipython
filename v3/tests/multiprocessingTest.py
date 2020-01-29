a326614699cc7fc1c028235fb4460626a920a566
Ramsey Karim <rkarim@astro.umd.edu>
2020-01-29 17:53:11 -0500

    one last tweak to multiproc test script

--- v3/tests/multiprocessingTest.py
+++ v3/tests/multiprocessingTest.py
@@ -1,6 +1,7 @@
 import os, sys
 import multiprocessing
 from time import sleep
+import numpy as np
 
 """
 A (less confusing) successor to the v2/multiprocessingTest
@@ -10,7 +11,7 @@ Created January 29, 2019
 """
 __author__ = "Ramsey Karim"
 
-def pfunc(t):
+def simple_parallel_func(t):
     print('module name:', __name__)
     if hasattr(os, 'getppid'):  # only available on Unix
         print('parent process:', os.getppid())
@@ -18,16 +19,28 @@ def pfunc(t):
     sleep(t)
     print('process id:', os.getpid(), "done sleeping")
 
-
-if __name__ == "__main__":
+def pfuncTest():
+    """
+    Run this under 'if name main'
+    """
     procs = []
     for i in range(4):
-        p = multiprocessing.Process(target=pfunc, args=(4,))
+        p = multiprocessing.Process(target=simple_parallel_func, args=(4,))
         p.start()
         procs.append(p)
     for p in procs:
         p.join()
-    pfunc(1)
+    simple_parallel_func(1)
+
+def array_parallel_func(i, total_i, arr):
+    raise NotImplementedError("not finished writing!!!")
+    print(f"Process number {i}, pid {os.getpid()}, starting...")
+    arr_slice_size = int(arr.size//total_i) + 1
+    arr = arr[slice(arr_slice_size*i, arr_slice_size*(i+1))]
+    ##### CONTINUE LATER
+
+if __name__ == "__main__":
+    array_parallel_func(None, None, None)
 
 """
 Actually I think this is finally making sense
