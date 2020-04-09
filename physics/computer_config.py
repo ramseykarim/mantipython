1f687c94d8da0f5349e5f0eaf04beceaf0fa5653
Ramsey Karim <rkarim@astro.umd.edu>
2020-04-09 15:22:56 -0400

    updated computer_config paths, commit right before i mess some things up

--- physics/computer_config.py
+++ physics/computer_config.py
@@ -9,5 +9,5 @@ p_RIMO = "/n/sgraraid/filaments/data/filterInfo_PlanckHerschel/HFI_RIMO_R3.00.fi
 
 if not os.path.isdir(bandpass_directory):
     # Laptop
-    bandpass_directory = "/home/ramsey/Documents/Research/Filaments/Herschel_bands/"
-    p_RIMO = "/home/ramsey/Documents/Research/Filaments/HFI_stuff/HFI_RIMO_R3.00.fits"
+    bandpass_directory = "/home/ramsey/Documents/Research/Filaments/filterInfo_PlanckHerschel/"
+    p_RIMO = "/home/ramsey/Documents/Research/Filaments/filterInfo_PlanckHerschel/HFI_RIMO_R3.00.fits"
--- tests/solveTest.py
+++ tests/solveTest.py
@@ -11,6 +11,7 @@ from ..physics import Greybody, TauOpacity, get_instrument
 """
 Test fit_full_image in solve_map.py
 Created 1/28/20, after renaming fit.py to solve_map.py
+Updated 4/9/20
 """
 __author__ = "Ramsey Karim"
 
@@ -26,31 +27,31 @@ be running smoothly.
 # Desktop path
 data_dir = "/sgraraid/filaments/data/TEST4/pacs70_cal_test/RCW49/processed/1342255009_reproc350/"
 if not os.path.isdir(data_dir):
-    # Laptop path
-    data_dir = "/home/ramsey/Documents/Research/Feedback/ancillary_data/herschel/"
+    # Laptop path (updated 4/9/20 after clean install)
+    data_dir = "/home/ramsey/Documents/Research/Feedback/ancillary_data/herschel/processed/1342255009/"
 data_fns = {
-    70: "PACS70um-image-remapped-conv-plus000184.fits", # -plus000102
-    160: "PACS160um-image-remapped-conv-plus000615.fits", # -plus000343
+    70: "PACS70um-image-remapped-conv-plus000080.fits", # -plus000102
+    160: "PACS160um-image-remapped-conv-plus000370.fits", # -plus000343
     250: "SPIRE250um-image-remapped-conv.fits",
     350: "SPIRE350um-image-remapped-conv.fits",
     # 500: "SPIRE500um-image-remapped-conv.fits",
 }
 err_fns = {
-    70: "PACS70um-error-remapped-conv.fits",
-    160: "PACS160um-error-remapped-conv.fits",
-    250: "SPIRE250um-error-remapped-conv.fits",
-    350: "SPIRE350um-error-remapped-conv.fits",
+    70: "PACS70um-error-remapped-conv-plus6.0pct.fits",
+    160: "PACS160um-error-remapped-conv-plus8.0pct.fits",
+    250: "SPIRE250um-error-remapped-conv-plus5.5pct.fits",
+    350: "SPIRE350um-error-remapped-conv-plus5.5pct.fits",
     # 500: "SPIRE500um-error-remapped-conv.fits",
 }
 mask = None # fits.getdata(data_dir+"dim_region_mask.fits").astype(bool)
-wavelens = [70, 160, 250, 350]
+wavelens = [70, 160, 250,]
 # assume we've set up arguments for a valid Cutout2D
 # these would be (center_i, center_j), (full_width_i, full_width_j)
 
 # i0, j0 = 726, 466
 # width_i, width_j = 220, 280
 
-i0, j0 = 150, 150
+i0, j0 = 450, 450
 width_i, width_j = 4, 4
 cutout_args = ((j0, i0), (width_i, width_j))
 
